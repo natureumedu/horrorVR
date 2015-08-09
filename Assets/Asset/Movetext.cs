2c78a6ddf042fed179b305df8d9c7823cde35e41
natureumedu <julyumedu@gmail.com>
2015-08-10 02:02:09 +0900

    first up

---
+++ Assets/Asset/Movetext.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movetext : MonoBehaviour {
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.Translate.y*s;
+		transform.Translate(0, 0.01f*speed, 0);
+	}
+}
